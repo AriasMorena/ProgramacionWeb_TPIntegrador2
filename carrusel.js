e109539aafea2d78b92d297c5d2b7d9ce164ff0d
guada <bguadalupemilagros@gmail.com>
2025-11-13 18:21:01 -0300

    cambios en las 3 paginas

---
+++ carrusel.js
@@ -0,0 +1,28 @@
+const imagenes = [
+    "img/Img1.jpeg",
+    "img/Img2.jpeg",
+    "img/Img3.jpeg",
+    "img/Img4.jpeg",
+    "img/Img5.jpeg",
+    "img/Img6.jpeg",
+    "img/Img7.jpeg"
+];
+
+let contador = 0;
+const imagenElemento = document.getElementById("carousel-image");
+
+function cambiarImagen(direccion) {
+    contador += direccion;
+
+
+if(contador < 0) {
+      contador = imagenes.length - 1;
+} else if (contador >= imagenes.length){
+    conatdor = 0;
+}
+
+imagenElemento.src = imagenes[contador];
+
+}
+
+cambiarImagen(0);
